2a9c0b6025bd3770a04718a861385875f27c7b69
René Vogt <rvogt@aki-gmbh.de>
2019-11-28 09:36:26 +0100

    Initial UI setup

---
+++ Kaboom/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Com.Revo.Games.Kaboom
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}

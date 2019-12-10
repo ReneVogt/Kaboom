dffdf6747fee1ee5f86a589f1587b963b52cda91
René Vogt <rvogt@aki-gmbh.de>
2019-12-10 11:14:11 +0100

    Started Unit-Tests for KaboomFieldSolver

--- KaboomEngineTests/KaboomTests/KaboomFieldSolverTests/SolveTests.cs
+++ KaboomEngineTests/KaboomTests/KaboomFieldSolverTests/SolveTests.cs
@@ -1,4 +1,7 @@
+using System;
 using Com.Revo.Games.KaboomEngine.Kaboom;
+using Com.Revo.Games.KaboomEngine.Kaboom.Fakes;
+using Com.Revo.Games.KaboomEngine.Wrapper.Fakes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace KaboomEngineTests.KaboomTests.KaboomFieldSolverTests
@@ -6,9 +9,44 @@ namespace KaboomEngineTests.KaboomTests.KaboomFieldSolverTests
     public partial class KaboomFieldSolverTests
     {
         [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
         public void Solve_NoField_ArgumentNullException()
         {
-        //    var sut = new KaboomFieldSolver()
+            var sut = new KaboomFieldSolver(new StubIGenerateConstraints(), new StubIProvideRandom());
+            // ReSharper disable once AssignNullToNotNullAttribute
+            sut.Solve(null, 0, 0);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Solve_NegativeX_ArgumentOutOfRangeException()
+        {
+            var sut = new KaboomFieldSolver(new StubIGenerateConstraints(), new StubIProvideRandom());
+            KaboomField field = new KaboomField(10, 10, 5, sut);
+            sut.Solve(field, -1, 0);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Solve_TooGreatX_ArgumentOutOfRangeException()
+        {
+            var sut = new KaboomFieldSolver(new StubIGenerateConstraints(), new StubIProvideRandom());
+            KaboomField field = new KaboomField(10, 10, 5, sut);
+            sut.Solve(field, 10, 0);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Solve_NegativeY_ArgumentOutOfRangeException()
+        {
+            var sut = new KaboomFieldSolver(new StubIGenerateConstraints(), new StubIProvideRandom());
+            KaboomField field = new KaboomField(10, 10, 5, sut);
+            sut.Solve(field, 0, -1);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Solve_TooGreatY_ArgumentOutOfRangeException()
+        {
+            var sut = new KaboomFieldSolver(new StubIGenerateConstraints(), new StubIProvideRandom());
+            KaboomField field = new KaboomField(10, 10, 5, sut);
+            sut.Solve(field, 0, 10);
         }
     }
 }

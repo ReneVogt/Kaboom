850b90b178a485a4f9094316a5a33189cd3e9553
René Vogt <rvogt@aki-gmbh.de>
2019-12-13 10:16:36 +0100

    removed email, changed random

--- KaboomEngine/Kaboom/KaboomRandom.cs
+++ KaboomEngine/Kaboom/KaboomRandom.cs
@@ -9,8 +9,7 @@ namespace Com.Revo.Games.KaboomEngine.Kaboom
         static readonly Random random = new Random();
 
         public int Next(int max) => Math.Max(0, Math.Min(max - 1, (int)(max * Normalize(random.NextDouble()))));
-        //static double Normalize(double rand) => 1 - Math.Sqrt(Math.Sqrt(rand));
-        static double Normalize(double rand) => 1 - Math.Sqrt(Math.Cos(0.5 * Math.PI * rand));
-        //static double Normalize(double rand) => 1 - Math.Cos(0.5 * Math.PI * rand);
+        //static double Normalize(double rand) => 1 - Math.Sqrt(Math.Cos(0.5 * Math.PI * rand));
+        static double Normalize(double rand) => 1 - Math.Sqrt(rand);
     }
 }
